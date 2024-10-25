b2e7eb77adc26a42d1273eb185176f6f58a3f860
user <user@test.net>
2024-10-25 21:23:26 +0300

    FE pro - jquery lesson 2 finished

--- scripts/FE_pro_lesson_2_jquery.js
+++ scripts/FE_pro_lesson_2_jquery.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
         }, 'slow', 'swing', () => {alert('done')});
     })
     $( "#accordion" ).accordion();
+    $('#show_dialog').click(function(){
+        $( "#dialog" ).dialog();
+    })
+
+    $('#hide_all').click(function(){
+        $('#jquery_content').slideToggle()
+    })
+
+
+
+    $('.single-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
 })
